b40f9e5d75af01d1c1207547d625888527fdbdaa
Alysson Vicuna de Oliveira <alyssontkd@gmail.com>
2019-08-22 08:53:18 -0300

    [UPDATE]
    UPDATE

--- app/src/actions/configs/config.php
+++ app/src/actions/configs/config.php
@@ -2,7 +2,7 @@
 $tiposBanco = array('mysql', 'mssql');
 
 define('DB_TYPE_SGBD', $tiposBanco[0]);
-define('DB_HOST', 'mysql-pipeline');
+define('DB_HOST', 'mysql-petshop');
 define('DB_NAME', 'petshop');
 define('DB_USER', 'root');
 define('DB_PASS', '12345678');
